46176e265ae8251b31e97da64b8b56e3a88060ba
Davide Aversa <thek3nger@gmail.com>
2020-04-28 17:06:47 +0200

    feat: shuffle things to support Swift package format
    enable github workflows

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "suncalc-swift",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "suncalc-swift",
+            targets: ["suncalc-swift"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "suncalc-swift",
+            dependencies: []),
+        .testTarget(
+            name: "suncalc-swiftTests",
+            dependencies: ["suncalc-swift"]),
+    ]
+)
---
+++ Tests/suncalc-swiftTests/Test.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Davide Aversa on 28/04/2020.
+//
+
+import Foundation
+import XCTest
+ 
+class DummyTests: XCTestCase {
+    func testTest() {
+        XCTAssertEqual("test", "test", "Incorrect title")
+    }
+}
