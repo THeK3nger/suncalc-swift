1a6701e2c5d6a384f703558d38e1d7d7ffe213b5
Davide Aversa <thek3nger@gmail.com>
2020-04-29 15:28:53 +0200

    refactor: remove old deprecated functions

--- Sources/suncalc-swift/utils/PositionUtils.swift
+++ Sources/suncalc-swift/utils/PositionUtils.swift
@@ -37,30 +37,5 @@ class PositionUtils {
         }
         return 0.0002967 / tan(hCalc + 0.00312536 / (hCalc + 0.08901179));
     }
-    
-    @available(swift, deprecated: 1.0, message: "will soon become unavailable. Use getRightAscension instead.")
-    class func getRightAscensionL(l:Double, b:Double) -> Double {
-        return PositionUtils.getRightAscension(l: l, b: b)
-    }
-    
-    @available(swift, deprecated: 1.0, message: "will soon become unavailable. Use getDeclination instead.")
-    class func getDeclinationL(l:Double, b:Double) -> Double {
-        return PositionUtils.getDeclination(l: l, b: b)
-    }
-    
-    @available(swift, deprecated: 1.0, message: "will soon become unavailable. Use getAzimuth instead.")
-    class func getAzimuthH(h:Double, phi:Double, dec:Double) -> Double {
-        return PositionUtils.getAzimuth(h: h, phi: phi, dec: dec)
-    }
-    
-    @available(swift, deprecated: 1.0, message: "will soon become unavailable. Use getAltitude instead.")
-    class func getAltitudeH(h:Double, phi:Double, dec:Double) -> Double {
-        return PositionUtils.getAltitude(h: h, phi: phi, dec: dec)
-    }
-    
-    @available(swift, deprecated: 1.0, message: "will soon become unavailable. Use getSiderealTime instead.")
-    class func getSiderealTimeD(d:Double, lw:Double) -> Double {
-        return PositionUtils.getSiderealTime(d: d, lw: lw)
-    }
 
 }
